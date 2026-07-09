80107541acdf5ab600e28c4478195d1e873ed9ee
arshil692-prog <muktars692@gmail.com>
2026-07-09 15:10:32 +0530

    types

---
+++ data_types.py
@@ -0,0 +1,38 @@
+a = 55
+b = 57
+c = 5 + 6j
+
+print("Numeric data types: ")
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Sequence types
+a = "Hello"
+b = [1, 2, 3, 4, "arshil", "saba"]
+c = (100, 200, 300)
+
+print("Sequences Date Types :")
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Example of set data type
+x = 12
+y = 40
+
+result = x + y
+
+print("result :", result)
+print("Data type of result :", type(result))
+
+# Types of variable
+
+name_str = "arshil"
+print("String to list :", list(name_str))
+
+my_list = [23, 24, 28]
+print("list to Tuple :", tuple(my_list))
+
+call_log = "8180961820"
+print("string to list:", list(str(call_log)))
