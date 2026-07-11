db2df22933a87cf498374cc4e82a154a91815991
arshil692-prog <muktars692@gmail.com>
2026-07-11 22:10:34 +0530

    stings ex

--- stings.py
+++ stings.py
@@ -29,3 +29,53 @@ print(name.strip())
 # 5. replace()
 print("\n5. replace()")
 print(text.replace("Python", "Java"))
+# 6. find()
+print("\n6. find()")
+print(text.find("Programming"))
+print(text.find("Java"))
+
+# 7. upper()
+print("\n7. upper()")
+print(text.upper())
+
+# 8. lower()
+print("\n8. lower()")
+print(text.lower())
+
+# 9. capitalize()
+print("\n9. capitalize()")
+print("python".capitalize())
+
+# 10. title()
+print("\n10. title()")
+print("python programming".title())
+
+# 11. count()
+print("\n11. count()")
+print(text.count("m"))
+
+# 12. startswith()
+print("\n12. startswith()")
+print(text.startswith("Python"))
+
+# 13. endswith()
+print("\n13. endswith()")
+print(text.endswith("Programming"))
+
+# 14. len()
+print("\n14. len()")
+print(len(text))
+
+# 15. in operator
+print("\n15. in operator")
+print("Python" in text)
+print("Java" in text)
+
+# 16. Escape Characters
+print("\n16. Escape Characters")
+
+print("Hello\nWorld")
+print("Name\tAge")
+print("C:\\Users\\Muktar")
+print("He said \"Hello\"")
+print('It\'s Python')
