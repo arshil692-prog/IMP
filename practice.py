df2ad039d0ada9e78696fe478935911c7a034633
arshil692-prog <muktars692@gmail.com>
2026-07-22 17:32:32 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,39 @@
+cube = {x:x**3 for x in range (1,6)}
+print(cube)
+
+
+def welcome():
+    print("Welcome")
+
+welcome()
+
+
+def class_student():
+    print("student")
+
+class_student()
+
+
+def greet(name):
+    print("hello",name)
+
+greet("arshil")   
+
+
+def class_A(division):
+    print("very good student of this division",division)
+
+class_A(10)
+
+college = "kgdm niphad"
+def class_B():
+    print(college)
+
+class_B()
+
+def class_C():
+    print(college)
+
+class_C()
+
+
